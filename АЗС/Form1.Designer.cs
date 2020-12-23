98fb073054f0eeee522949f05ab192aa18ba5c5a
AlexeyRyjkov <kawai4ik98@gmail.com>
2020-12-23 19:52:39 +0300

    Добавьте файлы проекта.

---
+++ АЗС/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace АЗС
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aquamarine;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(603, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(155, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Aquamarine;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel2.Location = new System.Drawing.Point(603, 270);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(155, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(572, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(575, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(174, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(174, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(174, 217);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.Location = new System.Drawing.Point(174, 293);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.Location = new System.Drawing.Point(174, 369);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(816, 488);
+            this.MinimumSize = new System.Drawing.Size(816, 488);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ АЗС/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace АЗС
+{
+    public partial class Form1 : Form
+    {
+        private bool place1, place2;
+        private List<Image> images = new List<Image>();
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<Panel> panels = new List<Panel>();
+        private bool isOn;
+        private Semaphore sem1 = new Semaphore(1, 1);
+        private Semaphore sem2 = new Semaphore(1, 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+            place1 = true;
+            place2 = true;
+            foreach (Control control in this.Controls)
+            {
+                if (control is PictureBox)
+                {
+                    var box = control as PictureBox;
+                    images.Add(box.BackgroundImage);
+                    pictureBoxes.Add(box);
+                }
+
+                if (control is Panel)
+                {
+                    panels.Add(control as Panel);
+                }
+            }
+
+            pictureBoxes.ForEach(box => box.BackgroundImage = default);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isOn)
+            {
+                var rng = new Random();
+                pictureBoxes.ForEach(box => box.BackgroundImage = images[rng.Next(0, pictureBoxes.Count)]);
+                isOn = true;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+            }
+            else
+            {
+                isOn = false;
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+            }
+        }
+
+        void checkPlace()
+        {
+            if (place1)
+            {
+                var car = getBox();
+                Thread thread = new Thread(() => fillCar(car, "panel1"));
+                thread.Start();
+            }
+            else
+            {
+                if (place2)
+                {
+                    var car = getBox();
+                    Thread thread = new Thread(() => fillCar(car, "panel2"));
+                    thread.Start();
+                }
+            }
+        }
+
+        PictureBox getBox()
+        {
+            var rng = new Random();
+            PictureBox box;
+            do
+            {
+                box = pictureBoxes[rng.Next(0, pictureBoxes.Count)];
+            } while (box.BackgroundImage == default);
+
+            return box;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            checkPlace();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var rng = new Random();
+            pictureBoxes.ForEach(box =>
+            {
+                if (box.BackgroundImage==default)
+                {
+                    box.BackgroundImage = images[rng.Next(0, images.Count)];
+                }
+            });
+        }
+
+        void fillCar(PictureBox car, string namePlace)
+        {
+            if (namePlace.Equals("panel1"))
+            {
+                sem1.WaitOne();
+                place1 = false;
+                Image image = car.BackgroundImage;
+                car.BackgroundImage = default;
+                var rng = new Random();
+                panel1.BackgroundImage = image;
+                Thread.Sleep(rng.Next(1, 6) * 1000);
+                panel1.BackgroundImage = default;
+                place1 = true;
+                sem1.Release();
+            }
+            if (namePlace.Equals("panel2"))
+            {
+                sem2.WaitOne();
+                place2 = false;
+                Image image = car.BackgroundImage;
+                car.BackgroundImage = default;
+                var rng = new Random();
+                panel2.BackgroundImage = image;
+                Thread.Sleep(rng.Next(1, 6) * 1000);
+                panel2.BackgroundImage = default;
+                place2 = true;
+                sem2.Release();
+            }
+        }
+    }
+}
