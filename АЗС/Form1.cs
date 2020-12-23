573ff1fd1e40af85f0ab50032f3c76d34c74494e
AlexeyRyjkov <kawai4ik98@gmail.com>
2020-12-23 20:09:01 +0300

    Рандомное время при создании очереди из авто, добавил коментарии

--- АЗС/Form1.cs
+++ АЗС/Form1.cs
@@ -13,13 +13,14 @@ namespace АЗС
 {
     public partial class Form1 : Form
     {
-        private bool place1, place2;
-        private List<Image> images = new List<Image>();
-        private List<PictureBox> pictureBoxes = new List<PictureBox>();
-        List<Panel> panels = new List<Panel>();
-        private bool isOn;
-        private Semaphore sem1 = new Semaphore(1, 1);
-        private Semaphore sem2 = new Semaphore(1, 1);
+        private bool place1, place2;//Флаги свободности колонки
+        private List<Image> images = new List<Image>();//Список с картинками машин
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();//Список пикчер боксов с формы
+        List<Panel> panels = new List<Panel>();//Список колонок
+        private bool isOn;//Включена/выключена заправка
+        private Semaphore sem1 = new Semaphore(1, 1);//Семафор для первой колонки
+        private Semaphore sem2 = new Semaphore(1, 1);//Семафор для второй колонки
+        private int timeNextCar = 1000;
 
         public Form1()
         {
@@ -45,14 +46,15 @@ namespace АЗС
         }
 
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//Запуск АЗС
         {
             if (!isOn)
             {
                 var rng = new Random();
-                pictureBoxes.ForEach(box => box.BackgroundImage = images[rng.Next(0, pictureBoxes.Count)]);
+                pictureBoxes.ForEach(box => box.BackgroundImage = images[rng.Next(0, pictureBoxes.Count)]);//Задаем первичную очередь из машин
                 isOn = true;
                 button1.Text = "Остановить";
+                timer2.Interval = timeNextCar;
                 timer1.Enabled = true;
                 timer2.Enabled = true;
             }
@@ -65,12 +67,12 @@ namespace АЗС
             }
         }
 
-        void checkPlace()
+        void checkPlace()//Проверка того есть ли свободные колонки для заправки и если есть отправка туда машины
         {
             if (place1)
             {
                 var car = getBox();
-                Thread thread = new Thread(() => fillCar(car, "panel1"));
+                Thread thread = new Thread(() => fillCar(car, "panel1"));//Запуск заправки в отдельном потоке
                 thread.Start();
             }
             else
@@ -78,13 +80,13 @@ namespace АЗС
                 if (place2)
                 {
                     var car = getBox();
-                    Thread thread = new Thread(() => fillCar(car, "panel2"));
+                    Thread thread = new Thread(() => fillCar(car, "panel2"));//Запуск заправки в отдельном потоке
                     thread.Start();
                 }
             }
         }
 
-        PictureBox getBox()
+        PictureBox getBox()//Вызов случайной машины для заправки
         {
             var rng = new Random();
             PictureBox box;
@@ -96,12 +98,12 @@ namespace АЗС
             return box;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)//Таймер который проверяет наличие свободного места на заправке раз в 100мс
         {
             checkPlace();
         }
 
-        private void timer2_Tick(object sender, EventArgs e)
+        private void timer2_Tick(object sender, EventArgs e)//Таймер заполняющий очередь раз в 100мс
         {
             var rng = new Random();
             pictureBoxes.ForEach(box =>
@@ -111,22 +113,24 @@ namespace АЗС
                     box.BackgroundImage = images[rng.Next(0, images.Count)];
                 }
             });
+            timeNextCar = new Random().Next(100, 3000);
+            timer2.Interval = timeNextCar;
         }
 
         void fillCar(PictureBox car, string namePlace)
         {
-            if (namePlace.Equals("panel1"))
+            if (namePlace.Equals("panel1"))//Если машина попала на первую заправку
             {
-                sem1.WaitOne();
-                place1 = false;
-                Image image = car.BackgroundImage;
-                car.BackgroundImage = default;
+                sem1.WaitOne();//Ожидание освобождения
+                place1 = false;//Установка флага занятости
+                Image image = car.BackgroundImage;//Получение картинки
+                car.BackgroundImage = default;//Установка дефолтной картинки на место машины в очереди
                 var rng = new Random();
                 panel1.BackgroundImage = image;
-                Thread.Sleep(rng.Next(1, 6) * 1000);
-                panel1.BackgroundImage = default;
-                place1 = true;
-                sem1.Release();
+                Thread.Sleep(rng.Next(1, 6) * 1000);//Генерация рандомного времени заправки и "Усыпление" потока
+                panel1.BackgroundImage = default;//После окончания заправки удаляем машину с колонки
+                place1 = true;//Установка флага свободности колонки
+                sem1.Release();//Выходим из семафора, чтобы другой мог его использовать
             }
             if (namePlace.Equals("panel2"))
             {
